e8863f37314e4de6f2ec9792324603fa72fcd66d
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-10-13 22:55:29 +0200

    new

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
